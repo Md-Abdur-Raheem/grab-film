3698548316c2080410566e9d69c9f7a2716e6b51
Md-Abdur-Raheem <md.raheem2450@gmail.com>
2021-09-26 11:51:54 +0600

    Header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className = "header">
+            <h2>Grab your seat now!!!</h2>
+            <h3>Total budget : $500</h3>
+        </div>
+    );
+};
+
+export default Header;
